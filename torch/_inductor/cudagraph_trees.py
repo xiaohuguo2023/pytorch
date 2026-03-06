76ca30ba17107b8cf3eea07d24504d33457d6c74
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-03-06 17:22:30 +0000

    Revert "[inductor] Add forward/backward mode to cudagraph tree log messages (#176556)"
    This reverts commit a5ad7677c814fbdd7b9f0c2b2fa8966fda07e6d0.
    
    Reverted https://github.com/pytorch/pytorch/pull/176556 on behalf of https://github.com/pytorch-auto-revert due to Reverted automatically by pytorch's autorevert, to avoid this behaviour add the tag autorevert: disable ([comment](https://github.com/pytorch/pytorch/pull/176556#issuecomment-4012999956))

--- torch/_inductor/cudagraph_trees.py
+++ torch/_inductor/cudagraph_trees.py
@@ -2247,10 +2247,9 @@ class CUDAGraphTreeManager:
                     if log.isEnabledFor(logging.DEBUG):
                         unexpected_rerecord_reason = status_logger()
                         log.debug(
-                            "[%s] Re-recording function=%s, mode=%s, reason=%s",
+                            "[%s] Re-recording function=%s, reason=%s",
                             self.compile_id,
                             self.get_func_name(function_id),
-                            self.id_to_mode[function_id].name,
                             unexpected_rerecord_reason,
                         )
                     else:
@@ -2339,10 +2338,9 @@ class CUDAGraphTreeManager:
         ):
             graph_id = self.new_graph_id()
             log.debug(
-                "[%s] Recording function=%s, mode=%s, cuda_graph_id=%d, inputs: %s",
+                "[%s] Recording function=%s, cuda_graph_id=%d, inputs: %s",
                 self.compile_id,
                 self.get_func_name(function_id),
-                self.id_to_mode[function_id].name,
                 graph_id.id,
                 format_inputs_log(new_inputs),
             )
@@ -2386,10 +2384,9 @@ class CUDAGraphTreeManager:
         func_name = self.get_func_name(function_id)
         if not already_warm:
             log.debug(
-                "[%s] Running warmup function=%s, mode=%s",
+                "[%s] Running warmup function=%s",
                 self.compile_id,
                 func_name,
-                self.id_to_mode[function_id].name,
             )
         else:
             log.debug(
