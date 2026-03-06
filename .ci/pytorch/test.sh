84eaaeb18bbf20f30c8de20cd72eab1251b96e25
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-03-06 21:40:08 +0000

    Revert "[BE] Do not checkout submodules during test jobs (#176655)"
    This reverts commit 8c246caf87c5b2c6d0527ad3852698a8f769a743.
    
    Reverted https://github.com/pytorch/pytorch/pull/176655 on behalf of https://github.com/pytorch-auto-revert due to Reverted automatically by pytorch's autorevert, to avoid this behaviour add the tag autorevert: disable ([comment](https://github.com/pytorch/pytorch/pull/176655#issuecomment-4014260013))

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -1851,7 +1851,6 @@ test_attention_microbenchmark() {
 }
 
 test_openreg() {
-  git submodule update --init --depth 1 third_party/googletest
   python test/run_test.py --openreg --verbose
   assert_git_not_dirty
 }
